4b5382a7d2893bb5e5a9301504abd71501d086b4
Mukund Kedia <mukundkedia7478@gmail.com>
2021-08-25 01:21:13 +0530

    first

---
+++ precache-manifest.93278cbb8e9514ee54381f2e755aac72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d4374f1bf374bee340f9f959f469be2",
+    "url": "/expense-manager/index.html"
+  },
+  {
+    "revision": "9040a8bc4bc0ca7c951e",
+    "url": "/expense-manager/static/css/main.6ebd63e0.chunk.css"
+  },
+  {
+    "revision": "72faef38c5de0f1ee358",
+    "url": "/expense-manager/static/js/2.4cf61f97.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/expense-manager/static/js/2.4cf61f97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9040a8bc4bc0ca7c951e",
+    "url": "/expense-manager/static/js/main.e31fbf94.chunk.js"
+  },
+  {
+    "revision": "ed1c2f1752361080ccf6",
+    "url": "/expense-manager/static/js/runtime-main.8b1b9801.js"
+  }
+]);
